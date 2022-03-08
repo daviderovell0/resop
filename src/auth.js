e9062c6ee9f5ca49f64d88e290bf6db31fd70a4e
daviderovell0 <davide.rovelli@ec-nantes.fr>
2022-03-08 15:50:59 +0100

    Allowing access to / without authentication

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,7 @@ import expressJwt from 'express-jwt';
 
 const authenticate = () =>
   expressJwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'] }).unless({
-    path: ['/auth/opn/login'],
+    path: ['/auth/opn/login', '/'],
   });
 
 export default { authenticate };
