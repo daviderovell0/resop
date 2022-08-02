2cdf53d0f2da528dd0c15a33b6086712bc7a9785
daviderovell0 <daviderovelli24@gmail.com>
2022-08-02 17:23:48 +0200

    implemented core/opn/getFolder + form fixes

--- src/operators/core/operations/getFile.js
+++ src/operators/core/operations/getFile.js
@@ -1,6 +1,6 @@
+import * as fs from 'fs';
 import Operation from '../../../backend/Operation';
 import SSHUtils from '../../../backend/SSHUtils';
-import * as fs from 'fs';
 
 const opn = new Operation();
 
@@ -8,8 +8,7 @@ opn.setDescription('Transfer file from the remote cluster to resop server');
 
 opn.defineOptions({
   source: 'full path (or relative path to $HOME to remote file',
-  destination:
-    'full path to local folder/file',
+  destination: 'full path to local folder/file',
 });
 
 function exec() {
@@ -20,25 +19,27 @@ function exec() {
 
   // check if source is a file
   let retobj = sshell.exec(`test -f ${opn.options.source}`, opn.user);
-  if(!retobj.success) { // append filename
-    opn.error(`${opn.options.source} not a file`)
+  if (!retobj.success) {
+    // append filename
+    opn.error(`${opn.options.source} not a file`);
   }
 
-  // if dest is an existing local dir, appenf source filename
+  // if dest is an existing local dir, append source filename
   if (fs.existsSync(opn.options.destination)) {
-    let idx = opn.options.source.lastIndexOf('/') +1;
-    if (idx === -1 ) idx = 0;
-    const filename = opn.options.source.slice(
-      idx,
-      opn.options.source.length
-    )
+    let idx = opn.options.source.lastIndexOf('/') + 1;
+    if (idx === -1) idx = 0;
+    const filename = opn.options.source.slice(idx, opn.options.source.length);
     opn.options.destination += `/${filename}`;
     console.log(opn.options.destination);
-}
+  }
 
-  retobj = sshell.scpRecv(opn.options.source, opn.options.destination, opn.user);
+  retobj = sshell.scpRecv(
+    opn.options.source,
+    opn.options.destination,
+    opn.user
+  );
 
-  if (!retobj.success) { 
+  if (!retobj.success) {
     // else throw generic error
     opn.error(retobj.output);
   }
--- src/operators/linux-user/operations/changePassword.js
+++ src/operators/linux-user/operations/changePassword.js
@@ -20,14 +20,14 @@ function exec() {
 
   opn.addLog('Hash generation: MD5...');
 
-  const passwordHash = await opn.runOperation(hashMD5, {
+  const passwordHash = opn.runOperation(hashMD5, {
     password: opn.options.password,
     escapeChar: 'true',
   });
   // escaping dollar characters created with MD5 encryption
   opn.addLog('changing password...');
-  await opn.runCommand(
-    `usermod --password "${passwordHash}" ${opn.options.username}`
+  opn.runCommand(
+    `/usr/sbin/usermod --password "${passwordHash}" ${opn.options.username}`
   );
   opn.addLog('done');
   return 'password changed';
